79a4b4152914c7ac0964592ea9dd2b121dcd1756
CongDanhTran <harrytran100@gmail.com>
2025-08-01 07:31:29 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ var sdpConstraints = {
 
 /////////////////////////////////////////////
 
-var room = 'foo';
+var room = '';
 // Could prompt for room name:
-// room = prompt('Enter room name:');
+room = prompt('Enter room name:');
 
 var socket = io.connect();
 
